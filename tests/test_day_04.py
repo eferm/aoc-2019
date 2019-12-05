485a3189ea1c5d3446aaab07678b4de5b4459525
eferm <emanuel@eferm.com>
2019-12-05 03:56:29 -0500

    tweak day4

--- tests/test_day_04.py
+++ tests/test_day_04.py
@@ -4,27 +4,27 @@ from ipynb.fs.defs.aoc.day_04 import _digits, adjacent_pt1, increase, adjacent_p
 
 def test_digits():
     digits = partial(_digits, 6)
-    assert not digits(1000)
-    assert not digits(10000)
-    assert digits(100000)
-    assert digits(999999)
-    assert not digits(1000000)
+    assert not digits('1000')
+    assert not digits('10000')
+    assert digits('100000')
+    assert digits('999999')
+    assert not digits('1000000')
 
 
 def test_adjacent_pt1():
-    assert adjacent_pt1(122345)
-    assert adjacent_pt1(123444)
-    assert not adjacent_pt1(123789)
-    assert adjacent_pt1(111111)
+    assert adjacent_pt1('122345')
+    assert adjacent_pt1('123444')
+    assert not adjacent_pt1('123789')
+    assert adjacent_pt1('111111')
 
 
 def test_increase():
-    assert increase(111123)
-    assert increase(135679)
-    assert not increase(223450)
+    assert increase('111123')
+    assert increase('135679')
+    assert not increase('223450')
 
 
 def test_adjacent_pt2():
-    assert adjacent_pt2(112233)
-    assert not adjacent_pt2(123444)
-    assert adjacent_pt2(111122)
+    assert adjacent_pt2('112233')
+    assert not adjacent_pt2('123444')
+    assert adjacent_pt2('111122')
